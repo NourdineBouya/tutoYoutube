f1cbbdfca5ea3cd796c3e606e4836148372a6fc2
nourdinebouya <nourdinebouya@gmail.com>
2023-08-07 14:14:03 +0100

    jai ajiuter du stle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='bg-yellow-500 px-20 py-10'>
         <Link href='/'> Acceuil</Link>
         <Link href='/contact'> Contact</Link>
         <Link href='/posts/123'> Single Post</Link>
